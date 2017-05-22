cb5eb2c97dac6c8d48499d46e8bfc33a7c6ee20f
salazar <luiz@crea-sc.org.br>
2017-05-22 17:11:16 -0300

    Delete contact via HTTP Delete

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   selector: 'app-contact',
@@ -8,9 +9,26 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ContactComponent implements OnInit {
 
   @Input() contact;
+  @Input() contacts;
 
-  constructor() { }
+  constructor(private _contactsService: ContactsService) { }
 
   ngOnInit() {
+  }
+
+  deleteContact(contact){
+    if (confirm("Você tem certeza que deseja excluir " + contact.firstName + " " + contact.lastName + "?")) {
+      var index = this.contacts.indexOf(contact);
+      this.contacts.splice(index, 1);
+
+      this._contactsService.deleteContact(contact.key)
+        .subscribe(
+          data => alert(data.message),
+          err => {
+            alert("Não foi possível remover o contato. Tente novamente!");
+            console.log(err);
+            this.contacts.splice(index, 0, contact);
+          });
+    }
   }
 }
